680116bc841078a2c0920124167c34ef01abf2ba
tomi77 <tomislav-ante.brncic@fer.hr>
2019-01-13 19:04:34 +0100

    Fix zadnjeg fixa

--- Model/TraktTv/TraktTVResponse.cs
+++ Model/TraktTv/TraktTVResponse.cs
@@ -18,7 +18,7 @@ namespace Model.TraktTv
     public class TraktTVResponse
     {
         public string title { get; set; }
-        public Int32 year { get; set; }
+        public Nullable<int> year { get; set; }
         public Ids ids { get; set; }
     }
 
